6f04013063277e0bbdd95ff3ecf86da28e8c31fd
wuxifei <wuxifei@finupgroup.com>
2020-01-08 18:57:35 +0800

    update config for socks-proxy

--- tools/socks-proxy/config.js
+++ tools/socks-proxy/config.js
@@ -1,13 +1,15 @@
 module.exports = {
   elif: {
     host: 'elif.site',
-    port: 2003,
-    matchs: [/google/]
+    port: 2080,
+    matchs: [/google/, /gist\.github\.com/, /chrome\.com/],
+    username: 'wxf',
+    password: 'wxf'
   },
   '10_10_80_242': {
     host: '10.10.80.242',
     port: 2080,
-    matchs: [/elif\.site/, /chrome\.com/],
+    matchs: [/elif\.site/, /pan\.baidu\.com/],
     username: 'wxf',
     password: 'wxf'
   },
