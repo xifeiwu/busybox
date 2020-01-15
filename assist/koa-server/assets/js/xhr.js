e85b6417ab3539659441b532778be5ef93c332e2
wuxifei <wuxifei@finupgroup.com>
2020-01-15 17:55:32 +0800

    hoist function dataFromJSONByContentType to utils/fe.js

--- assist/koa-server/assets/js/xhr.js
+++ assist/koa-server/assets/js/xhr.js
@@ -299,28 +299,6 @@ class Utils extends FEUtils {
     }
     return data;
   }
-  dataFromJSONByContentType(obj, contentType) {
-    var data = obj;
-    switch (contentType) {
-      case 'application/json':
-        break;
-      case 'application/x-www-form-urlencoded':
-        data = new URLSearchParams();
-        for (let key in obj) {
-          data.append(key, obj[key]);
-        }
-        data = data.toString();
-        break;
-      case 'multipart/form-data':
-        data = new FormData();
-        for (let key in obj) {
-          const value = obj[key];
-          Array.isArray(value) ? value.forEach(it => data.append(key, it)) : data.append(key, obj[key]);
-        }
-        break;
-    }
-    return data;
-  }
 }
 const utils = new Utils();
 
@@ -358,6 +336,7 @@ function xhrRequest(config) {
   if (config.path && config.path.startsWith('/') && !config.url) {
     config.url = location.origin + config.path;
   }
+  config.url = config.url ? config.url : config.path;
   config.data = utils.transformRequest(config.data, config.headers);
 
   return new Promise(function dispatchXhrRequest(resolve, reject) {
