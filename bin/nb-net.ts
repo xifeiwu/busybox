272e8f119974a696ae909b8c3c8b9ba0bbf2f112
xifei wu <xfwu@conviva.com>
2023-12-08 16:58:37 +0800

    add bin nb-net

---
+++ bin/nb-net.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env ts-node
+import path from 'path';
+import {spawnTsFile} from '../modules/lib/node';
+
+async function start() {
+  const argv = process.argv;
+  process.stdin.setRawMode(false);
+  const childProcess = spawnTsFile(path.resolve(__dirname, '../src/commander/net.ts'), {
+    aliasPath: true,
+    useTsconfig: true,
+    printCommand: true,
+    params: argv.length > 2 ? argv.slice(2) : [],
+    spawnOptions: {stdio: [0, 1, 2]},
+  });
+  childProcess.on('exit', () => {
+    process.stdin.setRawMode(true);
+  });
+}
+
+start();
---
+++ src/commander/net.ts
@@ -0,0 +1,14 @@
+import {Command} from 'commander';
+import {isPortOpen} from '@modules/lib/node';
+
+const program = new Command();
+
+program.name('net').description('utility for net handling');
+
+program.command('port-check <host> <port>').action(async (host, port, args, command) => {
+  console.log(host, port);
+  const isOK = await isPortOpen(host, port);
+  console.log(`isOK: ${isOK}`);
+});
+
+program.parse(process.argv);
