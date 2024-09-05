70b22ea3baf5965aeb22f268848ab34d7b0fdad1
xifei wu <xfwu@conviva.com>
2024-09-05 18:41:49 +0800

    combine process related command

--- src/commander/node-busybox/process.ts
+++ src/commander/node-busybox/process.ts
@@ -1,105 +1,51 @@
 import {Command} from 'commander';
 import {
-  getAllProcessInfo,
-  selectProcessToKill,
+  getProcessInfo,
   logColorful,
   getProcessInfoByPort,
-  toConsole,
-  getProcessTree,
-  ProcessInfoWithChildren,
+  ProcessFilter,
+  getFilterFunc,
+  ProcessInfoFilterFunc,
+  killProcessByPid,
+  ProcessInfo,
 } from '@modules/lib/node';
 
 export function appendProcessCommand(program: Command) {
   program
-    .command('infoByPort')
-    .argument('<port>', 'the port')
-    .action(async (port, options) => {
-      const processInfoList = await getProcessInfoByPort(port);
-      toConsole(processInfoList);
-    });
-  program
-    .command('killByPort')
-    .argument('<port>', 'the port')
-    .option('-p, --print', 'print process info or not')
-    .option('-s, --select', 'select the process to kill when more than on process exist')
-    .action(async (port, options) => {
-      const {print = true, select} = options;
-      const processInfoList = await getProcessInfoByPort(port);
-      await selectProcessToKill(processInfoList, {
-        printProcessInfo: print,
-        selectProcessToKill: select,
-      });
-    });
+    .command('pinfo [pid]')
+    .option('-i, --pid <pid>', 'pid filter')
+    .option('-c, --cmd <command>', 'command filter')
+    .option('-p, --port <port>', 'find process by port')
+    .option('-k, --kill', 'kill processes filtered out')
+    .action(async (argPid, options) => {
+      // console.log(options);
+      const {pid, cmd: command, port, tree, kill} = options;
+      const filter: ProcessFilter = {pid: pid ? pid : argPid, command};
+      const filterFunc = getFilterFunc(filter);
 
-  program
-    .command('infoByPid')
-    .argument('<pid>', 'pid of process')
-    .action(async pid => {
-      const info = await getProcessTree(pid);
-      logColorful({color: 'black'}, info);
-    });
-  program
-    .command('killByPid')
-    .argument('<pid>', 'pid of process')
-    .action(async pid => {
-      function killByPid(info: ProcessInfoWithChildren) {
-        const {pid, children} = info;
-        if (Array.isArray(children)) {
-          children.forEach(killByPid);
+      let pInfoByPort: ProcessInfo[];
+      if (port) {
+        pInfoByPort = await getProcessInfoByPort(port);
+      }
+      const finalFilter: ProcessInfoFilterFunc = info => {
+        if (port) {
+          if (pInfoByPort.length === 0) {
+            return false;
+          }
+          return Boolean(pInfoByPort.find(it => it.pid === info.pid));
         }
-        process.kill(Number(pid));
+        const {pid, ppid} = process;
+        return ![pid, ppid].includes(info.pid) && filterFunc(info);
+      };
+      const {allInfoList, pidToInfo} = await getProcessInfo({appendChildInfo: true});
+      const filteredInfoList = allInfoList.filter(finalFilter);
+      if (kill) {
+        await killProcessByPid(
+          filteredInfoList.map(it => it.pid),
+          {pidToInfo, doubleConfirm: true}
+        );
+      } else {
+        logColorful({color: 'black'}, filteredInfoList);
       }
-      const info = await getProcessTree(pid);
-      if (!info) {
-        throw new Error(`No process with pid ${pid}`);
-      }
-      killByPid(info);
-    });
-  program
-    .command('infoByCommand')
-    .argument('<cmdText>', 'cmdText of process')
-    .action(async cmdText => {
-      /** Ignore current process */
-      const pidIgnore = [process.pid, process.ppid];
-      const info = await getAllProcessInfo({
-        filter: info => {
-          if (!info) {
-            return false;
-          }
-          const {command, pid} = info;
-          if (!command) {
-            return false;
-          }
-          if (pidIgnore.includes(Number(pid))) {
-            return false;
-          }
-          return info.command && info.command.includes(cmdText);
-        },
-      });
-      logColorful({color: 'black'}, info);
-    });
-
-  program
-    .command('killByCommand')
-    .argument('<cmdText>', 'cmdText of process')
-    .action(async cmdText => {
-      /** Ignore current process */
-      const pidIgnore = [process.pid, process.ppid];
-      const info = await getAllProcessInfo({
-        filter: info => {
-          if (!info) {
-            return false;
-          }
-          const {command, pid} = info;
-          if (!command) {
-            return false;
-          }
-          if (pidIgnore.includes(Number(pid))) {
-            return false;
-          }
-          return info.command && info.command.includes(cmdText);
-        },
-      });
-      logColorful({color: 'black'}, info);
     });
 }
