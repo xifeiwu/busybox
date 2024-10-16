21ad4c019c9a2d67a9f29e8b79a18ebbdbd80788
xifei wu <xfwu@conviva.com>
2024-10-16 16:04:36 +0800

    update submodule

--- bin/login-to-server.ts
+++ bin/login-to-server.ts
@@ -3,10 +3,10 @@ import {spawn} from 'child_process';
 import {Transform} from 'stream';
 
 process.stdin.setRawMode(false); // Releasing stdin
-const childProcess = spawn('ssh', ['-tt', 'xifei@elif.site'], {stdio: [0,1,2]});
-childProcess.on("exit", function(code, signal) {
+const childProcess = spawn('ssh', ['-tt', 'xifei@elif.site'], {stdio: [0, 1, 2]});
+childProcess.on('exit', function (code, signal) {
   // Don't forget to switch pseudo terminal on again
-  process.stdin.setRawMode(true); 
+  process.stdin.setRawMode(true);
 });
 // childProcess.stdout.pipe(process.stdout, {end: false});
 // childProcess.stderr.pipe(process.stderr, {end: false});
