dee83f2aea963a919ae7a7f4b2a0f83b8dcbade0
xifei wu <xfwu@conviva.com>
2024-10-18 19:55:09 +0800

    update proxyConfigList for local socks server

--- src/config/tcp-gateway/local.ts
+++ src/config/tcp-gateway/local.ts
@@ -1,3 +1,31 @@
 import {TCP_GATEWAY_CONFIG} from '../../service/external';
 
+TCP_GATEWAY_CONFIG.mwConfig.socksConfig[5].proxyConfigList = [
+  {
+    socksVersion: 1,
+    auth: {
+      username: 'abc',
+      password: 'dddd',
+    },
+    socksServer: {
+      host: 'elif.site',
+      // host: '124.156.155.64',
+      port: 80,
+    },
+    matches: [
+      /google/,
+      /medium.com/,
+      /npmjs.com/,
+      /reddit.com/,
+      /quora.com/,
+      /bonus.ly/,
+      'stackoverflow.com',
+      'www.howtogeek.com',
+      /wikipedia/,
+      /v2ex.com/,
+      /youtube.com/,
+      /github.com/,
+    ],
+  },
+];
 export const localTcpGateWayConfig = TCP_GATEWAY_CONFIG;
