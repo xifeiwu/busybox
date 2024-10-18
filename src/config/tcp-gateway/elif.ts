28a15432d1d9c97c52814bb07daf3d9f1811527d
xifei wu <xfwu@conviva.com>
2024-10-18 08:45:25 +0800

    support socks by http upgrade

--- src/config/tcp-gateway/elif.ts
+++ src/config/tcp-gateway/elif.ts
@@ -7,32 +7,6 @@ import {
   uploadDirOnHome,
 } from '@src/service/external';
 
-export const koaConfig: KoaConfig = {
-  /** Make http server can be accessed from outside */
-  host: '127.0.0.1',
-  port: 8880,
-  bodyParserOptions: {
-    uploadDir: uploadDirOnHome,
-  },
-  mwConfig: {
-    ...mwConfigDefault,
-    staticWMConfig: {
-      spaDirList: [
-        {
-          fullpath: '/share/code/react/start/browser-feature/react-tsx-less/dist',
-          entries: ['net', 'browser-feature'],
-        },
-      ],
-    },
-  },
-  printOrigin: true,
-};
-
-export const tcpServerConfig: TcpServerConfig = {
-  port: 80,
-  host: '0.0.0.0',
-};
-
 export const SOCKS_SERVER_CONFIG: Partial<SocksServerConfigPerVersion> = {
   '1': {
     socksVersion: 1,
@@ -50,11 +24,39 @@ export const SOCKS_SERVER_CONFIG: Partial<SocksServerConfigPerVersion> = {
   //   ],
   // },
 };
+
+export const koaConfig: KoaConfig = {
+  /** Make http server can be accessed from outside */
+  host: '127.0.0.1',
+  port: 8880,
+  bodyParserOptions: {
+    uploadDir: uploadDirOnHome,
+  },
+  mwConfig: {
+    ...mwConfigDefault,
+    staticWMConfig: {
+      spaDirList: [
+        {
+          fullpath: '/share/code/react/start/browser-feature/react-tsx-less/dist',
+          entries: ['net', 'browser-feature'],
+        },
+      ],
+    },
+    socksConfig: SOCKS_SERVER_CONFIG,
+  },
+  printOrigin: true,
+};
+
+export const tcpServerConfig: TcpServerConfig = {
+  port: 80,
+  host: '0.0.0.0',
+};
+
 export const elifTcpGateWayConfig: TcpGateWayConfig = {
   tcpServerConfig: tcpServerConfig,
   mwConfig: {socksConfig: SOCKS_SERVER_CONFIG},
   middlewares: [],
   koa: {
-    config: koaConfig
-  }
+    config: koaConfig,
+  },
 };
