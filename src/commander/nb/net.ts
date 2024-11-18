65039d7ab2d1cf5471ac4d492e8ea96cddec8449
xifei wu <xfwu@conviva.com>
2024-11-18 11:39:02 +0800

    add command parse-href

--- src/commander/nb/net.ts
+++ src/commander/nb/net.ts
@@ -1,5 +1,6 @@
 import {Command} from 'commander';
 import {isPortOpen, getLocalIpAddress, logColorful} from '@src/service/external';
+import {toUrlProps} from '@modules/lib/node';
 
 export function appendNetCommand(program: Command) {
   program.command('port-check <host> [port]').action(async (host, port, args, command) => {
@@ -14,5 +15,9 @@ export function appendNetCommand(program: Command) {
   program.command('local-ip').action(async (host, port, args, command) => {
     const localIp = getLocalIpAddress();
     logColorful({}, localIp);
+  });
+  program.command('parse-href <href>').action(async href => {
+    const urlProps = toUrlProps(href);
+    logColorful({}, urlProps);
   });
 }
