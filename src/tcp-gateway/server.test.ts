0a41ad0a1eb2d9d93856e49303b43ea95f88c7f9
xifei wu <xfwu@conviva.com>
2024-12-09 16:56:31 +0800

    use port 3161 for local tcpGateway running

--- src/tcp-gateway/server.test.ts
+++ src/tcp-gateway/server.test.ts
@@ -7,7 +7,7 @@ export async function testStartTcpGatewayByOptions() {
   const env: Env = (process.env.HOME ?? 'local') as Env;
   const uploadDir = path.resolve(__dirname, 'uploads');
   const staticDir = undefined;
-  const tcpPort = undefined;
+  const tcpPort = 3161;
   const info = await startTcpGatewayByOptions({
     env,
     staticDir: staticDir ? path.resolve(process.cwd(), staticDir) : undefined,
