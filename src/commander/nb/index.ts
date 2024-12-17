ab34369778f136ba8d3c36642d3921c1862bc60f
xifei wu <xfwu@conviva.com>
2024-12-17 17:52:18 +0800

    add comment

--- src/commander/nb/index.ts
+++ src/commander/nb/index.ts
@@ -18,8 +18,9 @@ appendOtherCommand(program);
 
 const command = process.argv[2];
 if (command === 'pretty-curl') {
+  /** if command is pertty-curl, shift the command and pass the rest args to commander for parse and pretty curl command */
   const rest = process.argv.slice(3);
-  program.parse([...process.argv.slice(0, 2), ...rest])
+  program.parse([...process.argv.slice(0, 2), ...rest]);
 } else {
   program.parse(process.argv);
 }
