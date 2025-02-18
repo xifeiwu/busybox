34594239c8206978a0156d06469e0439e35b2793
xifei wu <xfwu@conviva.com>
2025-02-18 16:51:24 +0800

    use getDefaultHttpsConfig

--- src/daemon/script/tls-gateway.ts
+++ src/daemon/script/tls-gateway.ts
@@ -1,12 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import {
-  HttpRequestInfo,
-  startSocketClient,
-  startSocketServer,
-  getDefaultTlsConfig,
-} from '@src/service/external';
+import {HttpRequestInfo, startSocketClient, startSocketServer} from '@src/service/external';
 import {out, responseError} from './service';
+import {getDefaultHttpsConfig} from '@modules/lib/node';
 
 function route(requestInfo?: HttpRequestInfo) {
   return {
@@ -17,18 +11,11 @@ function route(requestInfo?: HttpRequestInfo) {
 
 export async function startTlsGateway() {
   try {
-    const certDir = path.resolve(process.env.HOME, '.ssh/elif.site');
-    const {server, host, port} = await startSocketServer(
-      async socket => {
-        const {host, port} = route();
-        const client = await startSocketClient({host, port});
-        socket.pipe(client).pipe(socket);
-      },
-      {
-        port: 443,
-        options: getDefaultTlsConfig(),
-      }
-    );
+    const {server, host, port} = await startSocketServer(async socket => {
+      const {host, port} = route();
+      const client = await startSocketClient({host, port});
+      socket.pipe(client).pipe(socket);
+    }, getDefaultHttpsConfig());
     out({host, port});
   } catch (err) {
     out(responseError(err));
