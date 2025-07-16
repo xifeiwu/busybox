8ab10cf47845b052ab4070ee227a9ed398a39460
xifei wu <xfwu@conviva.com>
2025-07-16 14:40:06 +0800

    suport custom runtime for bin generation

--- src/0-generate-command/1-generate-bin.ts
+++ src/0-generate-command/1-generate-bin.ts
@@ -4,18 +4,20 @@ import {DIR_PROJECT} from '../service';
 import {logColorful, getDir} from '../service/external';
 import {BIN_TO_COMMAND} from './config';
 
-function generateBinContent(binPath: string, cmdPath: string) {
+function generateBinContent(binPath: string, cmdPath: string, runtime: string) {
   if (!fs.existsSync(cmdPath)) {
     throw new Error(`cmdPath not found: ${cmdPath}`);
   }
   let relativePath = path.relative(getDir(binPath), cmdPath);
   const isTsFile = relativePath.endsWith('.ts');
 
+  /** Not need .ts suffix when import file */
   if (isTsFile) {
     relativePath = relativePath.substring(0, relativePath.length - 3);
   }
+  const finalRunTime = runtime ?? (isTsFile ? 'ts-node' : 'node');
   const content = [
-    `#!/usr/bin/env ${isTsFile ? 'ts-node' : 'node'}`,
+    `#!/usr/bin/env ${finalRunTime}`,
     isTsFile ? `import '${relativePath}'` : `require('${relativePath}')`,
     '',
   ].join('\n');
@@ -24,20 +26,17 @@ function generateBinContent(binPath: string, cmdPath: string) {
 }
 
 export async function generateBinFile() {
-  for (const [bin, cmdPath] of Object.entries(BIN_TO_COMMAND)) {
-    const tsBinPath = path.join(DIR_PROJECT, 'bin', bin + '.ts');
-    const tsCmdPath = path.join(DIR_PROJECT, 'src', cmdPath + '.ts');
-    try {
-      generateBinContent(tsBinPath, tsCmdPath);
-    } catch (err) {
-      logColorful({color: 'red'}, err.message);
-    }
-    const jsBinPath = path.join(DIR_PROJECT, 'bin', bin + '.js');
-    const jsCmdPath = path.join(DIR_PROJECT, 'dist', 'src', cmdPath + '.js');
-    try {
-      generateBinContent(jsBinPath, jsCmdPath);
-    } catch (err) {
-      logColorful({color: 'red'}, err.message);
-    }
+  for (const [bin, cmdInfo] of Object.entries(BIN_TO_COMMAND)) {
+    const {filePath: cmdFile, runtime} = cmdInfo;
+    generateBinContent(
+      path.join(DIR_PROJECT, 'bin', bin + '.ts'),
+      path.join(DIR_PROJECT, 'src', cmdFile + '.ts'),
+      runtime
+    );
+    generateBinContent(
+      path.join(DIR_PROJECT, 'bin', bin + '.js'),
+      path.join(DIR_PROJECT, 'dist', 'src', cmdFile + '.js'),
+      runtime
+    );
   }
 }
--- src/0-generate-command/config.ts
+++ src/0-generate-command/config.ts
@@ -1,8 +1,17 @@
+interface CmdInfo {
+  filePath: string;
+  runtime?: 'node' | 'ts-node';
+}
 /**
  * Not add extname of command, as the extname can be .ts or .js
  */
-export const BIN_TO_COMMAND: Record<string, string> = {
-  nb: 'mini-tools/command',
-  runTsExport: 'run-script/run-ts-export',
-  runTsScript: 'run-script/run-ts-script',
+export const BIN_TO_COMMAND: Record<string, CmdInfo> = {
+  nb: {filePath: 'mini-tools/command'},
+  runTsExport: {filePath: 'run-script/run-ts-export'},
+  runTsScript: {filePath: 'run-script/run-ts-script'},
+  'syncup-gitmodules': {
+    filePath: 'command/syncup-gitmodules',
+    /** It must run on ts-node, as this cmd need to import .ts file */
+    runtime: 'ts-node',
+  },
 };
