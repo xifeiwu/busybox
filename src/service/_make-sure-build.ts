acea14d7b8e5f0d10aeb2041ad61c9541e77adb3
xifei wu <xfwu@conviva.com>
2025-07-21 13:47:43 +0800

    import child file by main script

--- src/service/_make-sure-build.ts
+++
@@ -1,6 +0,0 @@
-/**
- * If .ts file not imported by this project, it will not compiled to .js file by tsc command.
- * The script run in child process is not imported directly by main script, it will not included in the dist folder.
- * So they should imported manually here
- */
-import {start} from '../../modules/lib/node/utils/run-script/run-in-cp/child';
