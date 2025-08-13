61e47cb52999933fbba6239a7c49ba85159a0f18
xifei wu <xfwu@conviva.com>
2025-08-13 11:04:14 +0800

    support more options for nb line-count

--- src/command/node-busybox/file.ts
+++ src/command/node-busybox/file.ts
@@ -13,48 +13,71 @@ export function appendFileCommand(program: Command) {
   program
     .command('line-count <filePath>')
     .option('-e, --exclude <exclude...>', 'exclude by relativePath')
+    .option('-l, --level <level>', 'exclude by relativePath')
+    .option('-f, --folder-only', 'kill processes filtered out')
     .description('show line count of a file or a directory')
-    .action(async (filePath, options) => {
-      function getFilter() {
-        const filterOutList: string[] = [];
-        const {exclude} = options;
-        let filter: FileFilter = () => true;
-        if (Array.isArray(exclude) && exclude.length > 0) {
-          const regList = exclude.map(it => new RegExp(it));
-          filter = ({relativePath}) => {
-            const noMatch = !regList.some(it => it.test(relativePath));
-            if (!noMatch) {
-              filterOutList.push(relativePath);
-            }
-            return noMatch;
-          };
+    .action(
+      async (
+        filePath,
+        options: {
+          exclude?: string[];
+          level?: string;
+          folderOnly?: boolean;
         }
-        return {fileFilter: filter, dirFilter: filter, filterOutList};
-      }
-      const {fileFilter, dirFilter, filterOutList} = getFilter();
-      const lineCountMap = getLineCountMap(filePath, {
-        fileFilter,
-        dirFilter,
-      });
-      const lineCountList = flatChildren(lineCountMap, {
-        includeDir: true,
-        sortChildren: (prev, next) => {
-          return next.lineCount - prev.lineCount;
-        },
-      });
-      const finalStr = lineCountList
-        .map(({relativePath, lineCount, children}) => {
-          return `${relativePath}${Array.isArray(children) ? '/' : ''}: ${lineCount}`;
-        })
-        .join('\n');
-      logColorful({color: 'black'}, finalStr);
-      if (filterOutList.length > 0) {
-        logColorful({color: 'red'}, `filter out file list:`);
-        for (const relativePath of filterOutList) {
-          logColorful({color: 'black'}, relativePath);
+      ) => {
+        const {exclude, folderOnly} = options;
+        let maxLevel: number;
+        if (options.level !== undefined) {
+          maxLevel = parseInt(options.level);
+          if (!Number.isInteger(maxLevel)) {
+            throw new Error(`param -l must be a number`);
+          }
+        }
+        function getFilter() {
+          const filterOutList: string[] = [];
+          let filter: FileFilter = () => true;
+          if (Array.isArray(exclude) && exclude.length > 0) {
+            const regList = exclude.map(it => new RegExp(it));
+            filter = ({relativePath}) => {
+              const noMatch = !regList.some(it => it.test(relativePath));
+              if (!noMatch) {
+                filterOutList.push(relativePath);
+              }
+              return noMatch;
+            };
+          }
+          return {fileFilter: filter, dirFilter: filter, filterOutList};
+        }
+        const {fileFilter, dirFilter, filterOutList} = getFilter();
+        const lineCountMap = getLineCountMap(filePath, {
+          fileFilter,
+          dirFilter,
+        });
+        const lineCountList = flatChildren(lineCountMap, {
+          includeDir: true,
+          sortChildren: (prev, next) => {
+            return next.lineCount - prev.lineCount;
+          },
+        });
+        const finalStr = lineCountList
+          .filter(it => {
+            const levelCheck = !Number.isInteger(maxLevel) || it.depth <= maxLevel;
+            const folderCheck = !folderOnly || Array.isArray(it.children);
+            return levelCheck && folderCheck;
+          })
+          .map(({relativePath, lineCount, depth, children}) => {
+            return `${relativePath}${Array.isArray(children) ? '/' : ''}: ${lineCount}`;
+          })
+          .join('\n');
+        logColorful({color: 'black'}, finalStr);
+        if (filterOutList.length > 0) {
+          logColorful({color: 'red'}, `filter out file list:`);
+          for (const relativePath of filterOutList) {
+            logColorful({color: 'black'}, relativePath);
+          }
         }
       }
-    });
+    );
 
   program
     .command('size <dir>')
