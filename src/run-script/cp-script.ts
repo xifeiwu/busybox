2abea06136cae64730feb484a3c7b13bd99faac1
xifei wu <xfwu@conviva.com>
2026-01-08 14:22:44 +0800

    remove dir src/run-script as it only contains command, the content is moved to src/1-command

--- src/run-script/cp-script.ts
+++
@@ -1,4 +0,0 @@
-/** import script in child process to make sure it's builded */
-import {start} from '../../modules/lib/node/utils/run-script/on-node/cp-wrap-script';
-
-start();
--- src/run-script/test/test.ts
+++
@@ -1,6 +0,0 @@
-export function calculate(n?: number) {
-  return (n ?? 5) + 5;
-}
-export function calculateStr(n?: number) {
-  return (n ?? 5) + '5';
-}
