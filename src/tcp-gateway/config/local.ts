6ef946b9545061f827e4451996bcffef4a92a902
xifei wu <xifei_wu@qq.com>
2026-06-03 18:54:21 +0800

    change the way of getting local tcpGatewayConfig

--- src/tcp-gateway/config/local.ts
+++ src/tcp-gateway/config/local.ts
@@ -1,62 +1,97 @@
 import path from 'path';
-import {PORT, SOCKS_AUTH_DEFAULT_USER_PASS, TCP_GATEWAY_DEFAULT_CONFIG} from '../../service/external';
+import {
+  PORT,
+  SOCKS_AUTH_DEFAULT_USER_PASS,
+  KoaConfig,
+  SocksServerConfigPerVersion,
+  TcpGateWayConfig,
+  TcpServerConfig,
+  DEFAULT_KOA_CONFIG,
+  SOCKS_SERVER_CONFIG,
+} from '../../service/external';
 
-TCP_GATEWAY_DEFAULT_CONFIG.tcpServerConfig.port = PORT.tcpGatewayServer.port;
-TCP_GATEWAY_DEFAULT_CONFIG.mwConfig.socks[5].proxyConfigList = [
-  {
-    socksVersion: 1,
-    auth: SOCKS_AUTH_DEFAULT_USER_PASS,
-    socksServer: {
-      host: 'elif.site',
-      // host: '124.156.155.64',
-      port: 80,
-    },
-    matches: [
-      /google/,
-      /medium.com/,
-      /nodejs.org/,
-      /npmjs.com/,
-      /reddit.com/,
-      /quora.com/,
-      /bonus.ly/,
-      'stackoverflow.com',
-      'www.howtogeek.com',
-      /wikipedia/,
-      /v2ex.com/,
-      /youtube.com/,
-      /github/,
+const localSocksConfig: Partial<SocksServerConfigPerVersion> = {
+  '1': SOCKS_SERVER_CONFIG['1'],
+  '5': {
+    ...SOCKS_SERVER_CONFIG['5'],
+    proxyConfigList: [
+      {
+        socksVersion: 1,
+        auth: SOCKS_AUTH_DEFAULT_USER_PASS,
+        socksServer: {
+          host: 'elif.site',
+          // host: '124.156.155.64',
+          port: 80,
+        },
+        matches: [
+          /google/,
+          /medium.com/,
+          /nodejs.org/,
+          /npmjs.com/,
+          /reddit.com/,
+          /quora.com/,
+          /bonus.ly/,
+          'stackoverflow.com',
+          'www.howtogeek.com',
+          /wikipedia/,
+          /v2ex.com/,
+          /youtube.com/,
+          /github/,
+        ],
+      },
     ],
   },
-];
-
-TCP_GATEWAY_DEFAULT_CONFIG.koa.config.port = PORT.stableHttpServer.port;
-TCP_GATEWAY_DEFAULT_CONFIG.koa.config.mwConfig.static = {
-  staticConfigList: [
-    {
-      dir: path.join(process.env.HOME, 'code/huffie/xifeiwu.github.io'),
-      // urlPrefix: '/resume',
-      fallbackUrl: {
-        '/resume': '/index.html',
-      },
-    },
-  ],
-  spaConfigList: [
-    {
-      dir: path.resolve(process.env.HOME, 'code/react/start/small-apps-wrapper/dist'),
-      entryToDistFile: {
-        '/browser-runtime/feature': '/browser-runtime/feature.html',
-        '/react-feature/feature': '/react-feature/feature.html',
-        '/forum': '/forum.html',
-        '/auth': '/auth.html',
-      },
-    },
-  ],
 };
 
-// TCP_GATEWAY_DEFAULT_CONFIG.tcpServerConfig.port = 3161;
-// TCP_GATEWAY_DEFAULT_CONFIG.mwConfig.assetsSyncUp = {
+const koaConfig: KoaConfig = {
+  ...DEFAULT_KOA_CONFIG,
+  port: PORT.stableHttpServer.port,
+  mwConfig: {
+    ...DEFAULT_KOA_CONFIG.mwConfig,
+    static: {
+      staticConfigList: [
+        {
+          dir: path.join(process.env.HOME, 'code/huffie/xifeiwu.github.io'),
+          // urlPrefix: '/resume',
+          fallbackUrl: {
+            '/resume': '/index.html',
+          },
+        },
+      ],
+      spaConfigList: [
+        {
+          dir: path.resolve(process.env.HOME, 'code/react/start/small-apps-wrapper/dist'),
+          entryToDistFile: {
+            '/browser-runtime/feature': '/browser-runtime/feature.html',
+            '/react-feature/feature': '/react-feature/feature.html',
+            '/forum': '/forum.html',
+            '/auth': '/auth.html',
+          },
+        },
+      ],
+    },
+    socks: localSocksConfig,
+  },
+};
+
+const tcpServerConfig: TcpServerConfig = {
+  host: '0.0.0.0',
+  port: PORT.tcpGatewayServer.port,
+};
+
+// tcpServerConfig.port = 3161;
+// assetsSyncUp: {
 //   dir: '/Users/Shared/assets',
 //   // git: 'git@elif.site:fe/module/assets.git',
-// };
+// },
 
-export const localTcpGateWayConfig = TCP_GATEWAY_DEFAULT_CONFIG;
+export const localTcpGateWayConfig: TcpGateWayConfig = {
+  tcpServerConfig,
+  mwConfig: {
+    socks: localSocksConfig,
+  },
+  middlewares: [],
+  koa: {
+    config: koaConfig,
+  },
+};
