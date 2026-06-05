8da5e030906fe180fccfd36a4d4676a39252fc45
xifei wu <xifei_wu@qq.com>
2026-06-05 17:50:02 +0800

    simplify cp script for tcp-gateway

--- src/tcp-gateway/cp-script.ts
+++ src/tcp-gateway/cp-script.ts
@@ -1,7 +1,4 @@
 import {
-  startSocketClient,
-  startSocketServer,
-  getDefaultHttpsConfig,
   Env,
   waitIpcMessageOnce,
   outputInfo,
@@ -14,38 +11,14 @@ interface IpcMessage {
   env: Env;
 }
 
-function route() {
-  return {
-    host: '127.0.0.1',
-    port: 80,
-  };
-}
-
-export async function startTlsGateway(config?: IpcMessage) {
-  try {
-    const {server, host, port} = await startSocketServer(async socket => {
-      const {host, port} = route();
-      const client = await startSocketClient({host, port});
-      socket.pipe(client).pipe(socket);
-    }, getDefaultHttpsConfig(config));
-    return {host, port};
-  } catch (err) {
-    return err.message;
-  }
-}
-
 export async function start() {
   const config = await waitIpcMessageOnce<IpcMessage>();
   try {
     const info = await startTcpGatewayByEnv(config);
     const response = serializeTcpGatewayInfo(info);
-    const tlsInfo = await startTlsGateway(config);
-    outputInfo({...response, tlsInfo}, {stdout: true, ipc: true});
+    outputInfo({...response}, {stdout: true, ipc: true});
   } catch (err) {
     outputInfo(getErrorMessage(err), {stdout: true, ipc: true});
   }
 }
 start();
-/**
- * In order to save resource cost on elif.site, startTlsGateway in the same process.
- */
